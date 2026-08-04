dccd04fe755228adfcc03ac5e5f2dc16a0347b2a
parikshit1218 <parikshitc76@gmail.com>
2026-08-04 23:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (80.31%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>=10){
+            int ans=0;
+        
+        
+        while(num>0){
+            int digit=num%10;
+         
+            ans=ans+digit;
+            num=num/10;
+           
+        }
+         num=ans;
+         
+        }
+        return num;
+       
+        
+    }
+};
