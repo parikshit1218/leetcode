aba8865cc54fa1bb10fb3a5fa51116a37047df43
parikshit1218 <parikshitc76@gmail.com>
2026-09-10 22:41:31 +0530

    Time: 39 ms (45.91%) | Memory: 72.7 MB (28.46%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,51 +1,19 @@
 class Solution {
 public:
-
     int findKthLargest(vector<int>& nums, int k) {
 
-        int n = nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-        // kth largest = (n-k)th smallest
-        int target = n - k;
+        for (int num : nums) {
 
-        int left = 0;
-        int right = n - 1;
+            pq.push(num);
 
-        while (left <= right) {
-
-            // Choose a random pivot
-            int pivotIndex = left + rand() % (right - left + 1);
-            int pivot = nums[pivotIndex];
-
-            // Move pivot to the end
-            swap(nums[pivotIndex], nums[right]);
-
-            int i = left;
-
-            // Partition
-            for (int j = left; j < right; j++) {
-
-                if (nums[j] < pivot) {
-                    swap(nums[i], nums[j]);
-                    i++;
-                }
-            }
-
-            // Put pivot in its correct position
-            swap(nums[i], nums[right]);
-
-            // Check pivot position
-            if (i == target) {
-                return nums[i];
-            }
-            else if (i < target) {
-                left = i + 1;
-            }
-            else {
-                right = i - 1;
+            // Keep only k largest elements
+            if (pq.size() > k) {
+                pq.pop();
             }
         }
 
-        return -1;
+        return pq.top();
     }
 };
